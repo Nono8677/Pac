d7485a43a73e230f7ad8727cc7195516d8376da8
Nono8677 <90137544+Nono8677@users.noreply.github.com>
2026-04-30 11:28:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,19 @@
-const CACHE_NAME = 'pachecoin-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'pachecoin-v6';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((k) => {
+                if (k !== CACHE_NAME) return caches.delete(k);
+            }));
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
